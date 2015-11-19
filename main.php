0fe7666ba9e22cf2ba1b252a6b1eafbc73908388
David Monteiro <davidmonf@hotmail.com>
2015-11-18 22:27:06 -0200

    Alterações Layout
    Alterações comulativas do layout principal do portal, ainda incompletos.

--- main.php
+++ main.php
@@ -6,15 +6,13 @@
   <meta http-equiv="Content-Type" content="text/xhtml; charset=latin1_bin" />
 </head>
 <body>
-	<table width="600" align="center" border=1 class="conteudo">
-		<tr><td colspan=2><?php include("topo.php")?></td></tr>
-		<tr><td colspan=2><?php include("busca.php")?></td></tr>
-		<tr><td colspan=2><?php include("categorias.php")?></td></tr>
+	<table align="center"class="conteudo" style="background-image: url(/imagens/miolo_suporte.jpg); background-repeat: repeat-y; width: 1024px; border-collapse: collapse;">
+	<?php include("topo_completo.php")?>
 		<tr>
 	     <?php 
 			 $sql_top10 = "SELECT * FROM $tbl_name WHERE APROVADO <> '0' ORDER BY COUNT DESC LIMIT 10"; 			 
 			 $result = mysql_query($sql_top10, $conecta_banco) or print(mysql_error());  
-				echo "<td width=500>";
+				echo "<td width=500 style='padding-left:60px;'>";
 				echo "<table>";
 				echo "<tr><td><h3>Tópicos Mais visitados</h3></td></tr>";
 				while ($resultado = mysql_fetch_assoc($result)) 
@@ -26,7 +24,7 @@
 			echo "</table></td>";
 			mysql_close($conecta_banco);
 		?>
-		<td width=500 align=right style="text-align: justify;">
+		<td width=500 align=right" style='padding-right:60px;'s>
 			<b>Como utilizar a Central de Ajuda?</b>
 			<p>Procure a ajuda que necessita na barra de categorias, na caixa de busca acima ou ainda verifique se a ajuda que precisa está ao lado nos tópicos mais visitados.</p>
 			<p>É importante verificar se o seu problema já está disponível aqui, pois assim ele será solucionado mais rapidamente.</p>
@@ -35,7 +33,7 @@
 			<p>Você pode também <a href='sugestao.php' class="lnk">sugerir um tópico</a> para podermos criar o conteúdo e ajudar ainda mais os colaboradores do CIEE!</p><br>
 		</td>
 		</tr>
-		<tr><td colspan=2><?php include("baixo.php") ?></td></tr>
+		<tr><td colspan=2 height=244 background="imagens/baixo_suporte.jpg" style="background-repeat:no-repeat;"><div><h4>Clique <a href="http://sistemaru.cieesp.org.br/servicedesk/Login_rsu.asp" target="_blank" class="lnk">aqui</a> para abrir um chamado no Suporte</h4></div></td></tr>
 	</table>
 </body>
 </html>
--- topo.php
+++ topo.php
@@ -1,7 +1,3 @@
-<table border=0>
-<tr>
-<td align="center"><div><a href="main.php"><img src="imagens/logo_ciee.gif" alt="CIEE" width="175" border="0" style="float: middle; vertical-align: top;"></a></div></td>
-</tr>
 <?php
 include("seguranca.php");
 if (isset($_SESSION['usuarioID']) && ($_SESSION['usuarioTipo'] != 'ESTAG')){
@@ -15,17 +11,6 @@ else
 {
 echo ('<div style="position: relative; top: 55px; right: 10px; height: 0px;"><h6 align="right"><a href="login.php">Acesso restrito</a></h6></div>');
 }
-?>
-</tr>
-<tr>
-<td>
-<table cellspacing="0" cellpadding="0" bordercolor="#000000" border="0" align="center" width="1024">
-<tr>
-<td width="25" valign="top" bgcolor="#00008b" align="left" style="height: 10px">
-			<img id="Image2" border="0" src="imagens/repBorderLeft1.gif"></img>
-</td>
-<td bgcolor="#00008b"><h1 align="center" style="color:White;">
-<?php 
 if (isset($_SESSION['usuarioID']))
 { 
 echo('<a href="main.php" class="lnk_topo">Central de Ajuda</a> - <a href="main_suporte.php" class="lnk_topo_suporte">Base de Conhecimento</a></h1></td>');
@@ -34,12 +19,6 @@ else
 {
 echo ('<a href="main.php" class="lnk_topo">Central de Ajuda - Suporte CIEE</a></h1></td>');
 }
-?>
-<td width="25" valign="top" bgcolor="#00008b" align="right" style="height: 18px">
-			<img id="Image1" border="0" src="imagens/repBorderRight1.gif"></img>
-</td>
-</tr>
-<?php
 if (isset($_SESSION['usuarioID']) && $_SESSION['usuarioTipo']  == 'MASTER')
 {
 	include ("conexao.php");
@@ -49,12 +28,9 @@ if (isset($_SESSION['usuarioID']) && $_SESSION['usuarioTipo']  == 'MASTER')
 	$resultado_pendentes_central = mysql_query($sql_pendentes_central, $conecta_banco) or print(mysql_error());
 	if ((mysql_num_rows($resultado_pendentes) > 0) || (mysql_num_rows($resultado_pendentes_central) > 0))
 	{
-	echo ("<tr bgcolor='#ffff00'><td colspan='3' align='center'>");
+	//echo ("<tr bgcolor='#ffff00'><td colspan='3' align='center'>");
 	echo ("Existem artigos pendentes para aprova&ccedil;&atilde;o. Clique <a href='pendentes.php'>aqui</a> para verificar.");
-	echo ("</tr></td>");
+	//echo ("</tr></td>");
 	}
 }
 ?>
-</table>
-</tr>
-</table>
