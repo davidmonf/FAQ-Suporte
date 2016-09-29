c7a04b087b1d69fefa969094f7b4f7456d6e9758
David Monteiro <davidmonf@hotmail.com>
2016-09-29 02:23:45 -0300

    Delete conexao.php

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php 
-			$host="localhost";
-			$username="root";
-			$password="netuno1989";
-			$db_name="faq";
-			$tbl_name="topicos";
-			$conecta_banco = mysql_connect("$host", "$username", "$password")or die("Não posso conectar"); 
-			mysql_select_db("$db_name")or print(mysql_error());
-?>
